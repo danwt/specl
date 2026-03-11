cc13478cbcbc1d0c689a9b4d64f5cb151841ef99
danwt <30197399+danwt@users.noreply.github.com>
2026-03-11 14:35:14 +0100

    chore: remove specl/fuzz/
    699 files deleted

--- specl/fuzz/target/debug/deps/parse-7b383e47de0e872e.d
+++
@@ -1,5 +0,0 @@
-/Users/danwt/Documents/repos/specl/specl/fuzz/target/debug/deps/parse-7b383e47de0e872e.d: fuzz_targets/parse.rs
-
-/Users/danwt/Documents/repos/specl/specl/fuzz/target/debug/deps/libparse-7b383e47de0e872e.rmeta: fuzz_targets/parse.rs
-
-fuzz_targets/parse.rs:
--- specl/fuzz/target/debug/deps/specl_ts-3ba709334ec0bf8a.d
+++
@@ -1,6 +0,0 @@
-/Users/danwt/Documents/repos/specl/specl/fuzz/target/debug/deps/specl_ts-3ba709334ec0bf8a.d: /Users/danwt/Documents/repos/specl/specl/crates/specl-ts/src/lib.rs /Users/danwt/Documents/repos/specl/specl/crates/specl-ts/src/types.rs
-
-/Users/danwt/Documents/repos/specl/specl/fuzz/target/debug/deps/libspecl_ts-3ba709334ec0bf8a.rmeta: /Users/danwt/Documents/repos/specl/specl/crates/specl-ts/src/lib.rs /Users/danwt/Documents/repos/specl/specl/crates/specl-ts/src/types.rs
-
-/Users/danwt/Documents/repos/specl/specl/crates/specl-ts/src/lib.rs:
-/Users/danwt/Documents/repos/specl/specl/crates/specl-ts/src/types.rs:
